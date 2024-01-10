a9c0488f14828cb64feb3f88254c74eaecba2341
rgoogoonani <68652911+rgoogoonani@users.noreply.github.com>
2024-01-10 23:25:15 +0330

    Update StartUp.py

--- StartUp.py
+++ StartUp.py
@@ -26,6 +26,7 @@ with open("/Marzban/"+Mname+"/.env","w") as f:
 getCommandOutput("cd "+Mname,"utf-8")
 getCommandOutput('echo "nameserver 178.22.122.100" > /etc/resolv.conf',"utf-8")
 getCommandOutput('sudo echo "nameserver 178.22.122.100" > /etc/resolv.conf',"utf-8")
+getCommandOutput("pip3 install -r "+Mname+"/requirements.txt","utf-8")
 getCommandOutput("sudo pip3 install -r "+Mname+"/requirements.txt","utf-8")
 getCommandOutput('echo "nameserver 1.1.1.1" > /etc/resolv.conf',"utf-8")
 getCommandOutput('sudo echo "nameserver 1.1.1.1" > /etc/resolv.conf',"utf-8")
