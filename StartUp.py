99512a524451f14e06b89119242cee0941359393
rgoogoonani <68652911+rgoogoonani@users.noreply.github.com>
2024-01-12 14:09:04 +0330

    Update StartUp.py

--- StartUp.py
+++ StartUp.py
@@ -18,16 +18,16 @@ Port=input("Marzban Port: ")
 UserName=input("Marzban UserName: ")
 PassWord=input("Marzban PassWord: ")
 
-getCommandOutput("unzip MainFiles.zip","utf-8")
+getCommandOutput("sudo unzip MainFiles.zip","utf-8")
 getCommandOutput("mv MainFiles "+Mname,"utf-8")
 with open("/Marzban/"+Mname+"/.env","w") as f:
     f.writelines('UVICORN_PORT = '+Port+'\nSUDO_USERNAME = "'+UserName+'"\nSUDO_PASSWORD = "'+PassWord+'"')
     f.close()
 getCommandOutput("cd "+Mname,"utf-8")
-getCommandOutput('echo "nameserver 178.22.122.100" > /etc/resolv.conf',"utf-8")
-getCommandOutput("pip3 install -r "+Mname+"/requirements.txt","utf-8")
-getCommandOutput('echo "nameserver 1.1.1.1" > /etc/resolv.conf',"utf-8")
-getCommandOutput("cd "+Mname+" && alembic upgrade head","utf-8")
+getCommandOutput('sudo echo "nameserver 178.22.122.100" > /etc/resolv.conf',"utf-8")
+getCommandOutput("sudo pip3 install -r "+Mname+"/requirements.txt","utf-8")
+getCommandOutput('sudo echo "nameserver 1.1.1.1" > /etc/resolv.conf',"utf-8")
+getCommandOutput("cd "+Mname+" && sudo alembic upgrade head","utf-8")
 
 
 
@@ -37,6 +37,6 @@ getCommandOutput("cd "+Mname+" && alembic upgrade head","utf-8")
 with open("/etc/systemd/system/Marzban"+Mname+".service","w") as f:
     f.writelines("[Unit]\nDescription=Marzban"+Mname+"\n\n[Service]\n\nExecStart=python3 /Marzban/"+Mname+"/main.py\n\n[Install]\nWantedBy=multi-user.target")
     f.close()
-getCommandOutput("systemctl daemon-reload","utf-8")
-getCommandOutput("systemctl start Marzban"+Mname,"utf-8")
-getCommandOutput("systemctl enable Marzban"+Mname,"utf-8")
+getCommandOutput("sudo systemctl daemon-reload","utf-8")
+getCommandOutput("sudo systemctl start Marzban"+Mname,"utf-8")
+getCommandOutput("sudo systemctl enable Marzban"+Mname,"utf-8")
